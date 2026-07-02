99668fe6b31a205a2f033476ba7ee4f57b597d12
Dhanush <dhanushsmrd@gmail.com>
2026-07-02 21:15:26 +0530

    Basic logic for Tic-Tac_to web based game

---
+++ script.js
@@ -0,0 +1,94 @@
+
+var tds=document.querySelectorAll("#value");
+prev="X"
+function add(k){
+    var present=tds[k].value
+    if ((present=='')&&(prev=="O")){
+        tds[k].value="X"
+        prev="X"
+    }
+    else if ((present=='')&& (prev=="X")){
+        tds[k].value="O"
+        prev="O"
+    }
+if ((tds[0].value==tds[1].value)&&(tds[1].value==tds[2].value)){
+    if ((tds[0].value!='')&&(tds[1].value!='')&&(tds[2].value!='')) {
+        if ((tds[0].value=="X")||(tds[1].value=='X')||(tds[2].value=='X')){
+            alert("Player 1 Wins")
+        }
+        else{
+            alert("Player 2 Wins")
+        }
+    }   
+}
+else if ((tds[0].value==tds[3].value)&&(tds[3].value==tds[6].value)){
+    if ((tds[0].value!='')&&(tds[3].value!='')&&(tds[6].value!='')) {
+        if ((tds[0].value=="X")||(tds[3].value=='X')||(tds[6].value=='X')){
+            alert("Player 1 Wins")
+        }
+        else{
+            alert("Player 2 Wins")
+        }
+    } 
+}
+else if ((tds[6].value==tds[7].value)&&(tds[7].value==tds[8].value)){
+    if ((tds[6].value!='')&&(tds[7].value!='')&&(tds[8].value!='')) {
+        if ((tds[6].value=="X")||(tds[7].value=='X')||(tds[8].value=='X')){
+            alert("Player 1 Wins")
+        }
+        else{
+            alert("Player 2 Wins")
+        }
+    } 
+}
+else if ((tds[8].value==tds[5].value)&&(tds[5].value==tds[2].value)){
+    if ((tds[8].value!='')&&(tds[5].value!='')&&(tds[2].value!='')) {
+        if ((tds[8].value=="X")||(tds[5].value=='X')||(tds[2].value=='X')){
+            alert("Player 1 Wins")
+        }
+        else{
+            alert("Player 2 Wins")
+        } 
+    } 
+}
+else if ((tds[3].value==tds[4].value)&&(tds[4].value==tds[5].value)){
+    if ((tds[3].value!='')&&(tds[4].value!='')&&(tds[5].value!='')) {
+        if ((tds[3].value=="X")||(tds[4].value=='X')||(tds[5].value=='X')){
+            alert("Player 1 Wins")
+        }
+        else{
+            alert("Player 2 Wins")
+        }
+    } 
+}
+else if ((tds[1].value==tds[4].value)&&(tds[4].value==tds[7].value)){
+    if ((tds[1].value!='')&&(tds[4].value!='')&&(tds[7].value!='')) {
+        if ((tds[1].value=="X")||(tds[4].value=='X')||(tds[7].value=='X')){
+            alert("Player 1 Wins")
+        }
+        else{
+            alert("Player 2 Wins")
+        } 
+    } 
+}
+else if ((tds[0].value==tds[4].value)&&(tds[4].value==tds[8].value)){
+    if ((tds[0].value!='')&&(tds[4].value!='')&&(tds[8].value!='')) {
+        if ((tds[0].value=="X")||(tds[4].value=='X')||(tds[8].value=='X')){
+            alert("Player 1 Wins")
+        }
+        else{
+            alert("Player 2 Wins")
+        }
+    } 
+}
+else if ((tds[2].value==tds[4].value)&&(tds[4].value==tds[6].value)){
+    if ((tds[4].value!='')&&(tds[6].value!='')&&(tds[2].value!='')) {
+        if ((tds[4].value=="X")||(tds[6].value=='X')||(tds[2].value=='X')){
+            alert("Player 1 Wins")
+        }
+        else{
+            alert("Player 2 Wins")
+        }
+    } 
+}
+}
