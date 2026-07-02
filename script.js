77438f24e05775f5a931529df9aa676049caff45
Dhanush <dhanushsmrd@gmail.com>
2026-07-02 22:05:53 +0530

    Added CS responsiveness and altered the logic of JS script

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-
+var restart=document.querySelector("#restart");
+restart.addEventListener("click",reset);
 var tds=document.querySelectorAll("#value");
+function reset(){
+    alert("Are you Sure you want to Restart !!!")
+    for (i in tds){
+        tds[i].value=''
+    }
+}
 prev="X"
 function add(k){
     var present=tds[k].value
@@ -11,84 +18,91 @@ function add(k){
         tds[k].value="O"
         prev="O"
     }
-if ((tds[0].value==tds[1].value)&&(tds[1].value==tds[2].value)){
-    if ((tds[0].value!='')&&(tds[1].value!='')&&(tds[2].value!='')) {
-        if ((tds[0].value=="X")||(tds[1].value=='X')||(tds[2].value=='X')){
-            alert("Player 1 Wins")
-        }
-        else{
-            alert("Player 2 Wins")
-        }
-    }   
+    else{
+        tds[k].value=""
+    }
+    timer=setTimeout(check,2000);
 }
-else if ((tds[0].value==tds[3].value)&&(tds[3].value==tds[6].value)){
-    if ((tds[0].value!='')&&(tds[3].value!='')&&(tds[6].value!='')) {
-        if ((tds[0].value=="X")||(tds[3].value=='X')||(tds[6].value=='X')){
-            alert("Player 1 Wins")
-        }
-        else{
-            alert("Player 2 Wins")
-        }
-    } 
-}
-else if ((tds[6].value==tds[7].value)&&(tds[7].value==tds[8].value)){
-    if ((tds[6].value!='')&&(tds[7].value!='')&&(tds[8].value!='')) {
-        if ((tds[6].value=="X")||(tds[7].value=='X')||(tds[8].value=='X')){
-            alert("Player 1 Wins")
-        }
-        else{
-            alert("Player 2 Wins")
-        }
-    } 
-}
-else if ((tds[8].value==tds[5].value)&&(tds[5].value==tds[2].value)){
-    if ((tds[8].value!='')&&(tds[5].value!='')&&(tds[2].value!='')) {
-        if ((tds[8].value=="X")||(tds[5].value=='X')||(tds[2].value=='X')){
-            alert("Player 1 Wins")
-        }
-        else{
-            alert("Player 2 Wins")
+
+function check(){
+    if ((tds[0].value==tds[1].value)&&(tds[1].value==tds[2].value)){
+        if ((tds[0].value!='')&&(tds[1].value!='')&&(tds[2].value!='')) {
+            if ((tds[0].value=="X")||(tds[1].value=='X')||(tds[2].value=='X')){
+                alert("Player 1 Wins")
+            }
+            else{
+                alert("Player 2 Wins")
+            }
+        }   
+    }
+    else if ((tds[0].value==tds[3].value)&&(tds[3].value==tds[6].value)){
+        if ((tds[0].value!='')&&(tds[3].value!='')&&(tds[6].value!='')) {
+            if ((tds[0].value=="X")||(tds[3].value=='X')||(tds[6].value=='X')){
+                alert("Player 1 Wins")
+            }
+            else{
+                alert("Player 2 Wins")
+            }
         } 
-    } 
-}
-else if ((tds[3].value==tds[4].value)&&(tds[4].value==tds[5].value)){
-    if ((tds[3].value!='')&&(tds[4].value!='')&&(tds[5].value!='')) {
-        if ((tds[3].value=="X")||(tds[4].value=='X')||(tds[5].value=='X')){
-            alert("Player 1 Wins")
-        }
-        else{
-            alert("Player 2 Wins")
-        }
-    } 
-}
-else if ((tds[1].value==tds[4].value)&&(tds[4].value==tds[7].value)){
-    if ((tds[1].value!='')&&(tds[4].value!='')&&(tds[7].value!='')) {
-        if ((tds[1].value=="X")||(tds[4].value=='X')||(tds[7].value=='X')){
-            alert("Player 1 Wins")
-        }
-        else{
-            alert("Player 2 Wins")
+    }
+    else if ((tds[6].value==tds[7].value)&&(tds[7].value==tds[8].value)){
+        if ((tds[6].value!='')&&(tds[7].value!='')&&(tds[8].value!='')) {
+            if ((tds[6].value=="X")||(tds[7].value=='X')||(tds[8].value=='X')){
+                alert("Player 1 Wins")
+            }
+            else{
+                alert("Player 2 Wins")
+            }
         } 
-    } 
-}
-else if ((tds[0].value==tds[4].value)&&(tds[4].value==tds[8].value)){
-    if ((tds[0].value!='')&&(tds[4].value!='')&&(tds[8].value!='')) {
-        if ((tds[0].value=="X")||(tds[4].value=='X')||(tds[8].value=='X')){
-            alert("Player 1 Wins")
-        }
-        else{
-            alert("Player 2 Wins")
-        }
-    } 
-}
-else if ((tds[2].value==tds[4].value)&&(tds[4].value==tds[6].value)){
-    if ((tds[4].value!='')&&(tds[6].value!='')&&(tds[2].value!='')) {
-        if ((tds[4].value=="X")||(tds[6].value=='X')||(tds[2].value=='X')){
-            alert("Player 1 Wins")
-        }
-        else{
-            alert("Player 2 Wins")
-        }
-    } 
-}
+    }
+    else if ((tds[8].value==tds[5].value)&&(tds[5].value==tds[2].value)){
+        if ((tds[8].value!='')&&(tds[5].value!='')&&(tds[2].value!='')) {
+            if ((tds[8].value=="X")||(tds[5].value=='X')||(tds[2].value=='X')){
+                alert("Player 1 Wins")
+            }
+            else{
+                alert("Player 2 Wins")
+            } 
+        } 
+    }
+    else if ((tds[3].value==tds[4].value)&&(tds[4].value==tds[5].value)){
+        if ((tds[3].value!='')&&(tds[4].value!='')&&(tds[5].value!='')) {
+            if ((tds[3].value=="X")||(tds[4].value=='X')||(tds[5].value=='X')){
+                alert("Player 1 Wins")
+            }
+            else{
+                alert("Player 2 Wins")
+            }
+        } 
+    }
+    else if ((tds[1].value==tds[4].value)&&(tds[4].value==tds[7].value)){
+        if ((tds[1].value!='')&&(tds[4].value!='')&&(tds[7].value!='')) {
+            if ((tds[1].value=="X")||(tds[4].value=='X')||(tds[7].value=='X')){
+                alert("Player 1 Wins")
+            }
+            else{
+                alert("Player 2 Wins")
+            } 
+        } 
+    }
+    else if ((tds[0].value==tds[4].value)&&(tds[4].value==tds[8].value)){
+        if ((tds[0].value!='')&&(tds[4].value!='')&&(tds[8].value!='')) {
+            if ((tds[0].value=="X")||(tds[4].value=='X')||(tds[8].value=='X')){
+                alert("Player 1 Wins")
+            }
+            else{
+                alert("Player 2 Wins")
+            }
+        } 
+    }
+    else if ((tds[2].value==tds[4].value)&&(tds[4].value==tds[6].value)){
+        if ((tds[4].value!='')&&(tds[6].value!='')&&(tds[2].value!='')) {
+            if ((tds[4].value=="X")||(tds[6].value=='X')||(tds[2].value=='X')){
+                alert("Player 1 Wins")
+            }
+            else{
+                alert("Player 2 Wins")
+            }
+        } 
+    }
 }
